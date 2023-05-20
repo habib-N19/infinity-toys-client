54bb11922cc35275d6845b5558799de94fcdbd9e
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-20 11:37:04 +0600

    added delete method for a specific id toy

--- src/pages/MyToys/MyToyCard.jsx
+++ src/pages/MyToys/MyToyCard.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 const MyToyCard = ({ myToy }) => {
   const { _id, name, sellerName, sub_category, price, quantity, photo } = myToy
+
+  const handleToyDelete = id => {
+    fetch(`http://localhost:5000/myToys/${id}`, {
+      method: 'DELETE'
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
       <td class='w-32 p-4'>
@@ -22,7 +30,12 @@ const MyToyCard = ({ myToy }) => {
       </td>
       <td class='px-0 py-4'>
         <button className='btn btn-success mr-3'>Update</button>
-        <button className='btn btn-warning  '>x</button>
+        <button
+          onClick={() => handleToyDelete(_id)}
+          className='btn btn-warning px-6'
+        >
+          x
+        </button>
       </td>
     </tr>
   )
