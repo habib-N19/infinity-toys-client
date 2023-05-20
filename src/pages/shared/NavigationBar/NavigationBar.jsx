f0a4efd9ab6f6d6f72a6ed3023fa24dab5bde47c
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-21 03:13:27 +0600

    added conditional rendering for protect route names in nav bar

--- src/pages/shared/NavigationBar/NavigationBar.jsx
+++ src/pages/shared/NavigationBar/NavigationBar.jsx
@@ -5,7 +5,7 @@ import '../../../index.css'
 import { FaBars } from 'react-icons/fa'
 import logo from '../../../assets/logo.png'
 const NavigationBar = () => {
-  const { logOut } = useContext(AuthContext)
+  const { user, logOut } = useContext(AuthContext)
   const handleLogOut = () => {
     logOut()
   }
@@ -27,22 +27,26 @@ const NavigationBar = () => {
           All Toys
         </Link>
       </li>
-      <li>
-        <Link
-          className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-          to='/myToys'
-        >
-          My Toys
-        </Link>
-      </li>
-      <li>
-        <Link
-          className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-          to='/addNewToy'
-        >
-          Add a Toy
-        </Link>
-      </li>
+      {user && (
+        <>
+          <li>
+            <Link
+              className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+              to='/myToys'
+            >
+              My Toys
+            </Link>
+          </li>
+          <li>
+            <Link
+              className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+              to='/addNewToy'
+            >
+              Add a Toy
+            </Link>
+          </li>
+        </>
+      )}
       <li>
         <Link
           className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
@@ -64,29 +68,37 @@ const NavigationBar = () => {
             </span>
           </Link>
           <div className='flex md:order-2'>
-            <img
-              className='w-10 h-10 mr-2 rounded-full'
-              src='https://flowbite.com/docs/images/logo.svg'
-              alt='user photo'
-            />
+            {user && (
+              <img
+                className='w-10 h-10 mr-2 rounded-full'
+                src={user?.photoURL}
+                alt='user photo'
+              />
+            )}
+            {user ? (
+              <button
+                onClick={handleLogOut}
+                type='button'
+                className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+              >
+                Log Out
+              </button>
+            ) : (
+              <Link to='/login'>
+                {' '}
+                <button
+                  type='button'
+                  className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                >
+                  Sign In
+                </button>
+              </Link>
+            )}
 
-            <button
-              onClick={handleLogOut}
-              type='button'
-              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-            >
-              Log Out
-            </button>
             <div className='dropdown w-full'>
               <label tabIndex={0} className='btn btn-ghost md:hidden'>
                 <FaBars></FaBars>
               </label>
-              {/* <ul
-                tabIndex={0}
-                className=' dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
-              >
-                {navBarItems}
-              </ul> */}
             </div>
           </div>
           <div
