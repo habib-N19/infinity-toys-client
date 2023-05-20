b3b74994c2dad79dabbfcb947eecc147a8b3fd46
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-21 01:55:47 +0600

    added category tab style

--- src/pages/Home/SubCategory/SubCategory.jsx
+++ src/pages/Home/SubCategory/SubCategory.jsx
@@ -1,6 +1,7 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import React, { useEffect, useState } from 'react'
 import 'react-tabs/style/react-tabs.css'
+import { Link } from 'react-router-dom'
 
 const SubCategory = () => {
   const [toyData, setToyData] = useState([])
@@ -18,7 +19,7 @@ const SubCategory = () => {
     // console.log(data)
   }
   return (
-    <div>
+    <div className='w-10/12 mx-auto'>
       <Tabs>
         <TabList>
           <Tab onClick={() => handleCategory('marvel')}>Marvel</Tab>
@@ -27,23 +28,91 @@ const SubCategory = () => {
         </TabList>
 
         <TabPanel>
-          <h2>
+          <div className='flex gap-4'>
             {toyData.map(t => (
-              <p key={t._id}>{t.name}</p>
+              <div key={t._id}>
+                <div class='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                  <img
+                    class='p-8 rounded-t-lg'
+                    src={t.photo}
+                    alt='product image'
+                  />
+                  <div class='px-5 pb-5'>
+                    <h5 class='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+                      {t.name}
+                    </h5>
+                    <div class='flex items-center mt-2.5 mb-5'>{t.rating}</div>
+                    <div class='flex items-center justify-between'>
+                      <span class='text-3xl font-bold text-gray-900 dark:text-white'>
+                        ${t.price}
+                      </span>
+                      <Link class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                        View Details
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             ))}
-          </h2>
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
-          {toyData.map(t => (
-            <p key={t._id}>{t.sellerName}</p>
-          ))}
+          <div className='flex gap-4'>
+            {toyData.map(t => (
+              <div key={t._id}>
+                <div class='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                  <img
+                    class='p-8 rounded-t-lg'
+                    src={t.photo}
+                    alt='product image'
+                  />
+                  <div class='px-5 pb-5'>
+                    <h5 class='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+                      {t.name}
+                    </h5>
+                    <div class='flex items-center mt-2.5 mb-5'>{t.rating}</div>
+                    <div class='flex items-center justify-between'>
+                      <span class='text-3xl font-bold text-gray-900 dark:text-white'>
+                        ${t.price}
+                      </span>
+                      <Link class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                        View Details
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
-          {toyData.map(t => (
-            <p key={t._id}>{t.price}</p>
-          ))}
+          <div className='flex gap-4'>
+            {toyData.map(t => (
+              <div key={t._id}>
+                <div class='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+                  <img
+                    class='p-8 rounded-t-lg'
+                    src={t.photo}
+                    alt='product image'
+                  />
+                  <div class='px-5 pb-5'>
+                    <h5 class='text-xl font-semibold tracking-tight text-gray-900 dark:text-white'>
+                      {t.name}
+                    </h5>
+                    <div class='flex items-center mt-2.5 mb-5'>{t.rating}</div>
+                    <div class='flex items-center justify-between'>
+                      <span class='text-3xl font-bold text-gray-900 dark:text-white'>
+                        ${t.price}
+                      </span>
+                      <Link class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                        View Details
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </TabPanel>
       </Tabs>
     </div>
