a9610b37769a0980e6f8df1fe8ccc7c17868423c
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-21 18:49:12 +0600

    added error page design

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import svg from '../../assets/errorpage.svg'
 
 const ErrorPage = () => {
   return (
-    <div>
-      404 Not found go back to{' '}
-      <Link to='/' className='text-xl btn btn-primary'>
-        Home
-      </Link>
+    <div className='flex w-11/12 mx-auto justify-center items-center mt-20'>
+      <div className='w-1/2'>
+        <img src={svg} alt='' />
+      </div>
+      <div className='w-1/2 space-y-5'>
+        <h2 className='text-xl'>Oops....!</h2>
+        <h2 className='text-2xl'>404 Not found!! Go back to</h2>
+        <Link to='/' className='text-xl btn btn-primary'>
+          Home
+        </Link>
+      </div>
     </div>
   )
 }
